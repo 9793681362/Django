9e552b78b4ef2a98d4293179bd66fed9a421ca42
9793681362 <135099268+9793681362@users.noreply.github.com>
2024-05-13 15:59:35 +0800

    提交代码

--- Django/urls.py
+++ Django/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'), # 将根路径与 home 视图函数相关联
     path('api/', views.my_view, name='my_api'),
     path('api/test_data/', views.test_data_api, name='test_data_api'),
+    path('api/get_nav_value/', views.get_nav_value, name='get_nav_value')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,5 +15,21 @@ def test_data_api(request):
     ]
     return JsonResponse(data, safe=False)
 
+def get_nav_value(request):
+    nav_value = [
+        {'id': 0, 'nav': 'Home', 'content': '\uF425', 'route': '/home'},
+        {'id': 1, 'nav': 'English', 'content': '\uF658', 'route': '/english'},
+        {'id': 2, 'nav': 'Master', 'content': '\uF7CD', 'route': '/master'},
+        {'id': 3, 'nav': 'Javascript', 'content': '\uF74C', 'route': '/javascript'},
+        {'id': 4, 'nav': 'Vue', 'content': '\uF8C4', 'route': '/vue'},
+        {'id': 5, 'nav': 'Python', 'content': '\uF75C', 'route': '/python'},
+        {'id': 6, 'nav': 'Django', 'content': '\uF7CE', 'route': '/django'},
+        {'id': 7, 'nav': '', 'content': ''},
+        {'id': 8, 'nav': 'GitHub', 'content': '\uF3ED', 'route': '/github'},
+        {'id': 10, 'nav': 'Plane', 'content': '\uF1A2', 'route': 'plane'},
+        {'id': 11, 'nav': '', 'content': ''}
+    ]
+    return JsonResponse(nav_value, safe=False)
+
 def home(request):
     return JsonResponse({"message": "Welcome to the API!"})
